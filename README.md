59b177f47325f17744ee9eab51089cf6e14814f6
Saulo Carvalho <saulo_carvalho123@hotmail.com>
2024-09-19 22:31:39 -0300

    Adicionado o amount

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 ## Requisitos
 
 - [X] Deve ser possível criar uma conta
-- [] Deve ser possível buscar o extrato bancário do cliente
+- [X] Deve ser possível buscar o extrato bancário do cliente
 - [] Deve ser possível realizar um depósito
 - [] Deve ser possível realizar um saque
 - [] Deve ser possível realizar uma transferência via PIX
@@ -16,7 +16,8 @@
 ## Regras de negócio
 
 - [X] Não deve ser possível cadastrar uma conta com CPF já existente
-- [] Não deve ser possível fazer um depósito em uma conta não existente
+- [X] Não deve ser possível fazer um depósito em uma conta não existente
+- [] Não deve ser possível fazer um depósito menor que o depósito mínimo
 - [] Não deve ser possível buscar o extrato em uma conta não existente
 - [] Não deve ser possível fazer um saque em uma conta não existente
 - [] Não deve ser possível fazer um pix de/para uma conta não existente
--- src/app.ts
+++ src/app.ts
@@ -1,35 +1,17 @@
 import { randomUUID } from 'crypto'
-import express, { NextFunction, Request, Response } from 'express'
+import express from 'express'
 
-import { Customer } from './types/customer'
+import { verifyExistsAccount } from './middlewares/verify-exists-account'
+import { Customer, Statement } from './types/customer'
 
 const app = express()
 
 app.use(express.json())
 
 // Banco de dados em memória
-const customers: Customer[] = []
+export const customers: Customer[] = []
 
-// Middleware para verificar se existe um cliente com o CPF
-function verifyExistsAcountCpf(
-  request: Request,
-  response: Response,
-  next: NextFunction,
-) {
-  // Captura o CPF do route params
-  const { cpf } = request.params
-
-  // Busca o cliente filtrando pelo CPF recebido no params
-  const customer = customers.find((customer) => customer.cpf === cpf)
-  // Verifica se customer existe, caso não, responde um erro para o cliente
-  if (!customer) {
-    return response.status(400).json({ message: 'Customer not found' })
-  }
-
-  request.customer = customer
-  // Caso o cliente exista, o next faz com que o código continue executando normalmente
-  next()
-}
+const minAmountOfDeposit = 0.1
 
 // ROTA: Cria um novo cliente
 app.post('/account', (request, response) => {
@@ -62,15 +44,43 @@ app.post('/account', (request, response) => {
 })
 
 // Rota: Busca os dados de um cliente
-app.get('/account/:cpf', verifyExistsAcountCpf, (request, response) => {
+app.get('/account/:cpf', verifyExistsAccount, (request, response) => {
   const { customer } = request
   return response.json({ data: customer })
 })
 
 // Rota: Busca o extrato de um cliente
-app.get('/statement/:cpf', verifyExistsAcountCpf, (request, response) => {
+app.get('/statement/:cpf', verifyExistsAccount, (request, response) => {
   const { customer } = request
   return response.json({ data: customer.statement })
+})
+
+// Rota: Deposita um valor a um cliente
+app.post('/deposit/:cpf', verifyExistsAccount, (request, response) => {
+  const { customer } = request
+  const { description, amount } = request.body as {
+    description: string
+    amount: number
+  }
+
+  if (amount < minAmountOfDeposit) {
+    return response
+      .status(400)
+      .json({ message: 'Amount must be greater then the minimum amount' })
+  }
+
+  const statement: Statement = {
+    description,
+    amount,
+    type: 'credit',
+    createdAT: new Date(),
+  }
+
+  customer.statement.push(statement)
+
+  return response.json({
+    message: `Deposit worth ${amount} successfully made`,
+  })
 })
 
 export default app
